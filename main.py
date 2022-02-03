52fa4a0f962fdb088a3bea7851bea23ab2bf2445
ushikado <ushikado@gmail.com>
2022-02-04 01:18:54 +0900

    Return MP3, validate request, update config, debug

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import io
 import json
 import math
 import datetime
 import numpy as np
 import soundfile
 import torch
+import wave
+import lameenc
 
 import commons
 import utils
@@ -13,10 +16,17 @@ from text.symbols import symbols
 from text import text_to_sequence
 import phoneme_encoder
 
-import flask
-from google.cloud import storage, firestore
+if not "DEBUG" in os.environ:
+    import flask
+    from google.cloud import storage, firestore
+    document = firestore.Client().collection("speech-synthesis").document("speech-synthesis")
 
 initialized = False
+
+# config
+access_control_allow_origin = "https://ushikado.dithub.io"
+bitrate = 64
+model_gs_uri = "gs://speech-synthesis/yuyuyui-vits-model.pth"
 
 all_characters = [
     "結城 友奈", "東郷 美森", "犬吠埼 風", "犬吠埼 樹", "三好 夏凜",
@@ -29,7 +39,8 @@ all_characters = [
 
 
 def main(request):
-    update_timestamp("speech-synthesis", "speech-synthesis")
+    update_timestamp()
+    update_config()
     init()
 
     if request.method == "GET":
@@ -42,31 +53,42 @@ def main(request):
     return process_preflight(request)
 
 
-def update_timestamp(collection_name, document_name):
+def update_timestamp():
     global document
 
-    if "DEBUG" in os.environ:
-        return
+    if not "DEBUG" in os.environ:
+        document_dict = document.get().to_dict()
+        document_dict["timestamp"] = datetime.datetime.now()
+        document.set(document_dict)
 
-    try:
-        document
-    except NameError:
-        document = firestore.Client().collection(collection_name).document(document_name)
 
-    document.set({"timestamp": datetime.datetime.now()})
-    return
+def update_config():
+    global document, access_control_allow_origin, bitrate, model_gs_uri
+
+    if not "DEBUG" in os.environ:
+        config = document.get().to_dict()
+        access_control_allow_origin = config["Access-Control-Allow-Origin"]
+        bitrate = config["bitrate"]
+        model_gs_uri = config["model"]
 
 
 def init():
-    global initialized, hps, net_g, _
+    global initialized, encoder
     if initialized:
         return
 
-    phoneme_encoder.init()
+    if not "DEBUG" in os.environ:
+        phoneme_encoder.init()
 
-    if not os.path.isfile("/tmp/model.pth"):
+    load_model()
+
+
+def load_model():
+    global model_gs_uri,  hps, net_g, _
+    
+    if not "DEBUG" in os.environ:
         client = storage.Client()
-        client.download_blob_to_file("gs://speech-synthesis/yuyuyui-vits-model.pth", open("/tmp/model.pth", "wb"))
+        client.download_blob_to_file(model_gs_uri, open("/tmp/model.pth", "wb"))
     
     hps = utils.get_hparams_from_file("configs/yuyuyui.json")
 
@@ -84,9 +106,9 @@ def init():
 
 
 def process_preflight(request):
+    global access_control_allow_origin
     headers = {
-        'Access-Control-Allow-Origin': 'https://ushikado.github.io',
-        #'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': access_control_allow_origin,
         'Access-Control-Allow-Methods': 'POST',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Max-Age': '3600'
@@ -95,13 +117,50 @@ def process_preflight(request):
 
 
 def process_post(request):
-    global initialized, hps, net_g, _
-    
     request_json = request.get_json()
+    if "command" in request_json:
+        return process_command(request_json)
+    elif "character_name" in request_json and "text" in request_json:
+        return process_synthesis(request_json)
+    else:
+        return process_bad_request(request_json)
+
+
+def process_command(request_json):
+    global access_control_allow_origin
+    headers = {
+        'Access-Control-Allow-Origin': access_control_allow_origin,
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600'
+    }
+    if request_json["command"] == "load_model":
+        load_model()
+    else:
+        return process_bad_request(request_json)
+        
+    return ("ok", 200, headers)
+
+
+def process_bad_request(request_json):
+    global access_control_allow_origin
+
+    response = flask.make_response(flask.send_file("bad_request.mp3", mimetype="audio/mp3", as_attachment=False))
+    response.headers["Access-Control-Allow-Origin"] = access_control_allow_origin
+    return response
+    return ("", 400, headers)
+
+
+def process_synthesis(request_json):
+    global access_control_allow_origin, hps, net_g, _
     chara = request_json["character_name"]
     text = request_json["text"]
 
-    chara_id = all_characters.index(chara)
+    try:
+        chara_id = all_characters.index(chara)
+    except ValueError:
+        return process_bad_request(request_json)
+
     sid = torch.LongTensor([chara_id])
     stn_tst = get_text(text, hps)
 
@@ -110,10 +169,16 @@ def process_post(request):
         x_tst_lengths = torch.LongTensor([stn_tst.size(0)])
         audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.float().numpy()
 
-    tmp_file = "/tmp/voice.ogg"
-    soundfile.write(tmp_file, audio, hps.data.sampling_rate, subtype="VORBIS", format="OGG")
-    response = flask.make_response(flask.send_file(tmp_file, mimetype="audio/ogg", as_attachment=False))
-    response.headers["Access-Control-Allow-Origin"] = "https://ushikado.github.io"
+    pcm_buffer = io.BytesIO()
+    soundfile.write(pcm_buffer, audio, hps.data.sampling_rate, subtype="PCM_16", format="WAV")
+    pcm_buffer.seek(0)
+    mp3 = encode(pcm_buffer.read(), hps.data.sampling_rate)
+    
+    if "DEBUG" in os.environ:
+        return mp3
+
+    response = flask.make_response(flask.send_file(io.BytesIO(mp3), mimetype="audio/mp3", as_attachment=False))
+    response.headers["Access-Control-Allow-Origin"] = access_control_allow_origin
     return response
 
 
@@ -124,6 +189,18 @@ def get_text(text, hps):
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = torch.LongTensor(text_norm)
     return text_norm
+
+
+def encode(pcm, sampling_rate):
+    global bitrate
+
+    encoder = lameenc.Encoder()
+    encoder.silence()
+    encoder.set_bit_rate(bitrate)
+    encoder.set_in_sample_rate(sampling_rate)
+    encoder.set_channels(1)
+    encoder.set_quality(3)  # 2-highest, 7-fastest
+    return encoder.encode(pcm) + encoder.flush()
 
 
 if __name__ == "__main__":
@@ -138,4 +215,6 @@ if __name__ == "__main__":
             return self.data
     
     import sys
-    print(main(RequestStub( {"character_name": sys.argv[1], "text": sys.argv[2]} )))
+    out = main(RequestStub( {"character_name": sys.argv[1], "text": sys.argv[2]} ))
+    with open("out1.mp3", "bw") as fp:
+        fp.write(out)
