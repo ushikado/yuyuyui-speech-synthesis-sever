28f22ff2b856050d901a58d6d0de8ef488301c9d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 01:38:53 +0000

    Adding tarfile member sanitization to extractall()

--- phoneme_encoder.py
+++ phoneme_encoder.py
@@ -86,7 +86,26 @@ def init():
 
     print("Extracting {} to {}".format(download_path, extract_path))
     with tarfile.open(download_path, mode="r|gz") as tar:
-        tar.extractall(path=extract_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=extract_path)
     
     os.remove(download_path)
 
